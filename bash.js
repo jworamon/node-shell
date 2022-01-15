427268c1b1301a2e6f910a9ef1692f973dd62eb6
Woramon Jamjod <jworamon@gmail.com>
2022-01-15 14:19:35 -0500

    completed implementing `curl`

--- bash.js
+++ bash.js
@@ -1,19 +1,22 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
 const cat = require('./cat');
+const curl = require('./curl');
 
 process.stdout.write('prompt >');
 process.stdin.on('data', data => {
     let input = data.toString().trim();
     let cmd = input.split(' ')[0];
-    let fileName = input.split(' ')[1];
+    let fileOrURL = input.split(' ')[1];
     
     if (cmd === 'pwd') {
         pwd();
     } else if ( cmd === 'ls') {
         ls();
     } else if (cmd === 'cat') {
-        cat(fileName);
+        cat(fileOrURL);
+    } else if (cmd === 'curl') {
+        curl(fileOrURL);
     }
 });
 
